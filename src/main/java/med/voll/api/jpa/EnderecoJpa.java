ee418c5fa0bf47d94e35f6c7f6cf59ebcdb9f5d5
Luis Martins <chateadorn@gmail.com>
2023-02-10 13:17:36 -0300

    criando classe base para o enderecojpa

---
+++ src/main/java/med/voll/api/jpa/EnderecoJpa.java
@@ -0,0 +1,27 @@
+package med.voll.api.jpa;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.records.EnderecoRecord;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoJpa {
+
+    private String logradouro, bairro, cep, numero, complemento, cidade, uf;
+
+    public EnderecoJpa(EnderecoRecord record) {
+        this.logradouro = record.logradouro();
+        this.bairro = record.bairro();
+        this.cep = record.bairro();
+        this.numero = record.numero();
+        this.complemento = record.complemento();
+        this.cidade = record.cidade();
+        this.uf = record.uf();
+    }
+}
