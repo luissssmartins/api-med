8663ee2f7310981c14df8de66844e53707be5d39
Luis Martins <chateadorn@gmail.com>
2023-02-10 13:17:11 -0300

    salvando informação no sql

--- src/main/java/med/voll/api/controller/CadastroController.java
+++ src/main/java/med/voll/api/controller/CadastroController.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
+import med.voll.api.jpa.MedicoJpa;
 import med.voll.api.records.MedicoRecord;
+import med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cadastro")
 public class CadastroController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void handleCadastro(@RequestBody MedicoRecord medicoRecord)
     {
-        System.out.println(medicoRecord);
+        repository.save(new MedicoJpa(medicoRecord));
     }
 }
