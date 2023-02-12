ce9ac6f487978ff76c4373415b9beffe406167f6
Luis Martins <chateadorn@gmail.com>
2023-02-12 17:40:50 -0300

    atualizando controller

--- src/main/java/med/voll/api/controller/CadastroController.java
+++ src/main/java/med/voll/api/controller/CadastroController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.jpa.MedicoJpa;
+import med.voll.api.dto.Medico;
 import med.voll.api.records.MedicoRecord;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +19,6 @@ public class CadastroController {
     @PostMapping
     public void handleCadastro(@RequestBody MedicoRecord medicoRecord)
     {
-        repository.save(new MedicoJpa(medicoRecord));
+        repository.save(new Medico(medicoRecord));
     }
 }
