13bc606124839cdb3ec6a7acf7edd9b3906999ca
Luis Martins <chateadorn@gmail.com>
2023-02-13 12:06:39 -0300

    adicionando validações usando o bean validation

--- src/main/java/med/voll/api/records/medico/MedicoRecord.java
+++ src/main/java/med/voll/api/records/medico/MedicoRecord.java
@@ -1,7 +1,30 @@
 package med.voll.api.records.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.enums.Especialidade;
 import med.voll.api.records.endereco.EnderecoRecord;
 
-public record MedicoRecord(String nome, String email, String crm, Especialidade especialidade, EnderecoRecord endereco) {
+public record MedicoRecord(
+
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull
+        @Valid
+        EnderecoRecord endereco) {
 }
