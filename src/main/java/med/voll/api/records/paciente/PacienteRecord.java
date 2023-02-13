6791fbd6eca2a29a401a1a6914fe32a03f844a19
Luis Martins <chateadorn@gmail.com>
2023-02-13 12:22:16 -0300

    atualizando paciente record com o bean validation

--- src/main/java/med/voll/api/records/paciente/PacienteRecord.java
+++ src/main/java/med/voll/api/records/paciente/PacienteRecord.java
@@ -1,5 +1,31 @@
 package med.voll.api.records.paciente;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.records.endereco.EnderecoRecord;
-public record PacienteRecord(String nome, String dataDeNascimento, String idade, String cpf, EnderecoRecord enderecoRecord) {
+public record PacienteRecord(
+
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        String idade,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        String cpf,
+
+        @NotBlank
+        String telefone,
+
+        @NotNull
+        @Valid
+        EnderecoRecord endereco) {
 }
