9a37e135e8d9414592e3878edb07b5cecd3baa1e
Luis Martins <chateadorn@gmail.com>
2023-02-13 12:22:52 -0300

    registrando controller do paciente

--- src/main/java/med/voll/api/controller/CadastroPacienteController.java
+++ src/main/java/med/voll/api/controller/CadastroPacienteController.java
@@ -1,4 +1,27 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.entity.Paciente;
+import med.voll.api.records.paciente.PacienteRecord;
+import med.voll.api.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastro/paciente")
 public class CadastroPacienteController {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @PostMapping
+    @Transactional
+    public void handleCadastro(@RequestBody @Valid PacienteRecord pacienteRecord)
+    {
+        pacienteRepository.save(new Paciente(pacienteRecord));
+    }
 }
