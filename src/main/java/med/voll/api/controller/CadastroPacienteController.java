a1eee23cf3b72da53cb939879c39352b7b4c336f
Luis Martins <chateadorn@gmail.com>
2023-02-24 17:56:10 -0300

    pegando apenas pacientes ativos + deletando logicamente

--- src/main/java/med/voll/api/controller/CadastroPacienteController.java
+++ src/main/java/med/voll/api/controller/CadastroPacienteController.java
@@ -5,6 +5,7 @@ import jakarta.validation.Valid;
 import med.voll.api.entity.Paciente;
 import med.voll.api.records.paciente.PacienteRecord;
 import med.voll.api.records.paciente.PacientesRecord;
+import med.voll.api.records.paciente.UpdatePacienteRecord;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -29,7 +30,23 @@ public class CadastroPacienteController {
     @GetMapping
     public Page<PacientesRecord> handlePacientes(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable)
     {
-        return pacienteRepository.findAll(pageable).map(PacientesRecord::new);
+        return pacienteRepository.findAllByAtivoTrue(pageable).map(PacientesRecord::new);
 
+    }
+
+    @PutMapping
+    @Transactional
+    public void handleUpdate(@RequestBody @Valid UpdatePacienteRecord record)
+    {
+       Paciente paciente = pacienteRepository.getReferenceById(record.id());
+       paciente.update(record);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void handleDelete(@PathVariable Long id)
+    {
+        Paciente paciente = pacienteRepository.getReferenceById(id);
+        paciente.inactive();
     }
 }
