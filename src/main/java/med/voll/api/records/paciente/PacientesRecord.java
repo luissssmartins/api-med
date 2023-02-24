79f32717483c6bf4da0402e530a9792022881c78
Luis Martins <chateadorn@gmail.com>
2023-02-24 17:55:42 -0300

    adicionando no construtor id + get

--- src/main/java/med/voll/api/records/paciente/PacientesRecord.java
+++ src/main/java/med/voll/api/records/paciente/PacientesRecord.java
@@ -2,9 +2,9 @@ package med.voll.api.records.paciente;
 
 import med.voll.api.entity.Paciente;
 
-public record PacientesRecord(String nome, String idade, String cpf, String email) {
+public record PacientesRecord(Long id, String nome, String idade, String cpf, String email) {
 
     public PacientesRecord(Paciente paciente) {
-        this(paciente.getNome(), paciente.getIdade(), paciente.getCpf(), paciente.getEmail());
+        this(paciente.getId(), paciente.getNome(), paciente.getIdade(), paciente.getCpf(), paciente.getEmail());
     }
 }
